0dfba663851beaf65f9b8d17b94fc1c770129427
Nicolas <nicolas.goepp@epitech.eu>
2020-05-12 19:10:44 +0200

    added dark mode for logo, site style and material ui

--- pages/a.tsx
+++ pages/a.tsx
@@ -2,11 +2,13 @@ import { TextField } from "@material-ui/core";
 import React from "react";
 import '../i18n';
 import { useTranslation } from "react-i18next";
+import DarkModeParent from "../components/encapsulationComponents/DarkModeParent";
 
 const APage = () => {
   const [t] = useTranslation();
   return (
     <div>
+      <DarkModeParent>    
         <div>
           <form noValidate autoComplete="off">
             <div>
@@ -28,6 +30,7 @@ const APage = () => {
             </div>
           </form>
         </div>
+      </DarkModeParent>        
     </div>
   );
 };
---
+++ themes/site/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components'
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    body: string,
+    text: string,
+    toggleBorder: string,
+    gradient: string,
+  }
+}
