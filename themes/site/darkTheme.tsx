1c28b6b0ce68594101115e656e9e57d99194d83d
Nicolas <nicolas.goepp@epitech.eu>
2020-06-08 19:07:46 +0200

    darkTheme finished

---
+++ themes/site/darkTheme.tsx
@@ -0,0 +1,11 @@
+const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    gradient: 'linear-gradient(#091236, #1E215D)',
+    border : {
+        color: 'white'
+    }
+}
+
+export default darkTheme;
---
+++ themes/site/lightTheme.tsx
@@ -0,0 +1,11 @@
+const lightTheme = {
+    body: '#E2E2E2',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    gradient: 'linear-gradient(#39598A, #79D7ED)',
+    border : {
+        color: 'black'
+    }
+}
+
+export default lightTheme;
