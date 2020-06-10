047ded781cdd50fbd2527f9305cc676186d54eae
Tom Florentin <tom.florentin@epitech.eu>
2020-06-10 21:35:14 +0200

    search route

--- server/schema.ts
+++ server/schema.ts
@@ -3,6 +3,7 @@ import { gql } from "apollo-server-micro";
 const schema = gql`
   type Query {
     getTest: Success!
+    searchProducts(query: String!) : [Product]
   }
 
   type Mutation {
@@ -11,6 +12,21 @@ const schema = gql`
 
   type Success {
     message: String
+  }
+
+  type Product {
+    name: String!
+    brand: String
+    priceUnit: String!
+    priceMass: String
+    ingredients: [String]
+    packaging: [String]
+    allergens: [String]
+    nutriments: [String]
+    nutriscore: String
+    scoreHealth: Int
+    scoreEnvironment: Int
+    quantity: String
   }
 `;
 
