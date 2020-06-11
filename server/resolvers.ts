7e7c7ca9c49c2565612ba7d12c6ca30ecdab3def
Tom Florentin <tom.florentin@epitech.eu>
2020-06-11 17:52:28 +0200

    import error

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,11 +1,9 @@
 import { Product, Resolvers } from "../typing";
-import { productsResolvers } from "./products";
 import { algoQuery } from "./query";
 
 let testValue = "unset";
 
 const resolvers: Resolvers = {
-  ...productsResolvers,
   Query: {
     searchProducts: async (_obj, _args, _context, _info) => {
       return algoQuery<Product>("SELECT * FROM products WHERE name LIKE ? OR keywords LIKE ? LIMIT 10", [`%${_args.query}%`, `%${_args.query}%`]);
