e004adba23b0a9f2e38860ceda7c37089c04a6f2
Tom Florentin <tom.florentin@epitech.eu>
2020-09-08 15:08:08 +0200

    unit tests and fixes

--- server/account.test.ts
+++ server/account.test.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError, UserInputError } from "apollo-server-micro";
 import { endConnections } from "./query";
 import { Mutate, Query } from "./utils/tests";
 
@@ -9,10 +10,36 @@ afterAll(() => {
   endConnections();
 })
 
+
+describe('Basic errors cases with account', () => {
+  it('should deny access to account without being logged in', async () => {
+    await expect(Query("account", {})).rejects.toStrictEqual(new AuthenticationError("please login"));
+  })
+
+  it('should deny access with invalid username or password', async () => {
+    await expect(Mutate("login", { email: "invalid-email@test.com", passwordSHA256: "invalidPassword123" })).rejects.toStrictEqual(new AuthenticationError("Invalid email or password"));
+  })
+
+  it('should deny setting criterions or filters to a unlogged user', async () => {
+    await expect(Mutate("setObligations", { obligations: [] })).rejects.toStrictEqual(new AuthenticationError("please login"));
+    await expect(Mutate("setCriterions", { criterions: [] })).rejects.toStrictEqual(new AuthenticationError("please login"));
+  })
+
+  it('should deny the creation of a double account', async () => {
+
+    const duplicateEmail = `user-dup-${Math.floor(Math.random() * 1000)}@test.com`;
+    const token = await Mutate("register", { email: duplicateEmail, passwordSHA256: "rand" });
+    await expect(Mutate("register", { email: duplicateEmail, passwordSHA256: "rand" })).rejects.toStrictEqual(new AuthenticationError("Email already in use"));
+    await expect(Mutate("removeAccount", { passwordSHA256: "invalid" }, token)).rejects.toStrictEqual(new AuthenticationError("Invalid password"));
+    await expect(Mutate("removeAccount", { passwordSHA256: "rand" }, token)).resolves.toBeTruthy();
+  })
+})
+
 describe('Account', () => {
 
   beforeAll(async (done) => {
-    token = await Mutate("register", { email, passwordSHA256: "blbl" });
+    await Mutate("register", { email, passwordSHA256: "blbl" });
+    token = await Mutate("login", { email, passwordSHA256: "blbl" });
     done();
   })
 
@@ -41,5 +68,15 @@ describe('Account', () => {
     expect(newProfile.criterions.find((o: any) => o.position == 1).id).toBe(2);
     expect(newProfile.criterions.find((o: any) => o.position == 2).id).toBe(1);
     expect(newProfile.criterions.filter((o: any) => o.activated).length).toBe(2);
+  })
+
+  it('sould deny invalid ids into criterions and obligations', async () => {
+    await expect(Mutate("setCriterions", { criterions: [{ id: 9999, position: 1 }] }, token)).rejects.toStrictEqual(new UserInputError("Invalid criterion id"));
+    await expect(Mutate("setObligations", { obligations: [{ id: 9999 }] }, token)).rejects.toStrictEqual(new UserInputError("Invalid obligation id"));
+  })
+
+  it('sould deny invalid positions in criterions', async () => {
+    await expect(Mutate("setCriterions", { criterions: [{ id: 1, position: 0 }] }, token)).rejects.toStrictEqual(new UserInputError("Criterions position must start with 1"));
+    await expect(Mutate("setCriterions", { criterions: [{ id: 1, position: 1 }, { id: 2, position: 3 }] }, token)).rejects.toStrictEqual(new UserInputError("Criterions position are incorrect (not 1, 2, 3...)"));
   })
 })
