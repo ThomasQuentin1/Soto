c3b604fc62ee9e0ef38635b77bf1ae09c9992713
Nicolas Goepp <nicolas.goepp@epitech.eu>
2020-10-14 18:49:30 +0900

    Fix some details

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import DarkModeParent from "../components/encapsulationComponents/DarkModeParent";
 import { useDarkMode } from "../components/settings/useDarkMode";
 import Header from "components/global/Header";
-import Footer from "components/global/Footer";
 
 
 const LoginPage = () => {
