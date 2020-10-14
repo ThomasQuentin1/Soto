c29199e930c7eaa0453b6bee7ef6c272950f4745
Nicolas Goepp <nicolas.goepp@epitech.eu>
2020-10-14 16:12:02 +0900

    Footer now stay at bottom when no scroll is available

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDarkMode } from "../../components/settings/useDarkMode";
 
@@ -8,9 +8,15 @@ const Footer = () => {
     const tmpTheme: string = theme.toString();
     i18n;
     setTheme;
+    console.log(document.documentElement.scrollHeight);
+
+    const documentHeight = document.documentElement.scrollHeight;
+    // à changer mais pas mieux pour le moment; 754 -> taille du document; 118 -> taille actuelle du footer
+    const footerClassName = documentHeight <= 770 ? "footer-div-sticky" : "footer-div-scroll";
+    console.log(footerClassName)
 
     return (
-    <div className="footer-div" style={{left: '0px', top: '0px', width:'100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', paddingTop: '10px', paddingBottom: '10px'}}>
+    <div className={footerClassName} style={{width:'100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', paddingTop: '10px', paddingBottom: '10px'}}>
         <div></div>
         <img
             style={{left: '20px', marginLeft: "10px", height: '100px', width: '100px', marginRight: '10px'}}
