ad31371ad16e2537f1e7b08ca8ccf2e950de67fc
Tom Florentin <tom.florentin@epitech.eu>
2021-05-03 10:12:09 +0200

    vegan filter

--- server/algo/scoring/Vegan.ts
+++ server/algo/scoring/Vegan.ts
@@ -1,5 +1,11 @@
 import { DbProduct } from "server/dbSchema";
+import { veganUnallowedFood } from "../../constData/vegan";
 
-export const IsVegan = (_product: DbProduct): boolean => {
-  return Math.random() < 0.5;
+export const IsVegan = (product: DbProduct): boolean => {
+  const ing = product.ingredients.split("|");
+  return !ing.find(
+    (i) =>
+      veganUnallowedFood.en.find((v) => i.includes(v)) ||
+      veganUnallowedFood.fr.find((v) => i.includes(v))
+  );
 };
