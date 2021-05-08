6c2c8923bb553732379fedec9a437a9c2aacd336
Arnaud <arnaud.motte@epitech.eu>
2021-05-08 02:07:34 +0200

    Prepare for merge on dev

--- components/shop/SearchWrapper.tsx
+++ components/shop/SearchWrapper.tsx
@@ -2,13 +2,8 @@ import React from 'react'
 import ParametersSelect from "components/shop/ParametersSelect";
 import SearchBar from "components/shop/SearchBar";
 import SearchWrapperProps from "interfaces/SearchWrapper";
-import {useTranslation} from "react-i18next";
-import { Button } from '@material-ui/core';
-// import {useAccountQuery} from "../../typing";
 
 const SearchWrapper = ({ cartQuery } : SearchWrapperProps) => {
-
-    // if (!loading) {
     return (
         <div style={{display: 'flex', flexDirection: 'column'}}>
             <ParametersSelect/>
@@ -17,9 +12,6 @@ const SearchWrapper = ({ cartQuery } : SearchWrapperProps) => {
         </div>
     </div>
     );
-    // } else {
-    //     return <></>
-    // }
 }
 
 export default SearchWrapper
