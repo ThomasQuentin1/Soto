06ab76f075329b89fb20ce0cde7c10c6c2ee0c9b
Nicolas Goepp <nicolas.goepp@epitech.eu>
2021-05-08 21:00:55 +0900

    feat(shopPage): confirmCart mutation

--- components/shop/PriceBanner.tsx
+++ components/shop/PriceBanner.tsx
@@ -4,11 +4,11 @@ import {Button, Typography} from '@material-ui/core';
 import { useTranslation } from "react-i18next"
 import Container from '@material-ui/core/Container';
 import PriceBannerProps from 'interfaces/PriceBanner';
-import { Product } from 'typing';
+import { Product, useConfirmCartMutation } from 'typing';
 
 const calculateTotalPrice = (basket: Product[]) => {
     let totalPrice : number = 0;
-
+      
     if (basket) {
         basket.map((product) => {
             totalPrice += Number(product.priceUnit) * product.itemQuantity!;
@@ -21,6 +21,11 @@ const PriceBanner = ({basket} : PriceBannerProps) => {
     const totalPrice = calculateTotalPrice(basket);
     const [t] = useTranslation();
     
+    const [confirmCartMutation, {}] = useConfirmCartMutation({
+        variables: {
+        },
+    });
+
     return (
     <Container style={{display: 'flex', flexDirection:'row', width: '100%', alignItems: 'center', justifyContent:'center', alignContent: 'center', marginTop:'20px', padding: '10px'}} maxWidth={false} className='price_banner'>
         <Typography
@@ -33,8 +38,19 @@ const PriceBanner = ({basket} : PriceBannerProps) => {
             variant="h6"
             color="secondary"
             style={{marginLeft:'auto'}}
+            
         >Prix total : {totalPrice.toFixed(2)}€</Typography>
-        <Button color='secondary' variant="outlined" style={{marginLeft: '20px', fontSize:'17px'}}>{t('shop.payButton.label')}</Button>
+        <Button color='secondary' variant="outlined" 
+        onClick={() => {
+            confirmCartMutation().then((r) => {
+                if (r.errors) {
+                    console.log(r.errors[0].message)
+                } else {
+                    console.log("cart confirmed")
+                }
+            })
+        }}
+        style={{marginLeft: '20px', fontSize:'17px'}}>{t('shop.payButton.label')}</Button>
     </Container>)
 };
 
